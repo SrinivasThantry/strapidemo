32c194f0a23eb988f7e9f259c94760ce39276a56
SRINIVAS THANTRY <srinivas.thantry@hitachivantara.com>
2020-11-03 20:19:23 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b38cbbd18fe0241701525b6998a68b9f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bc1a83af-7d03-4439-ac43-6de4ab377a90'
+};
